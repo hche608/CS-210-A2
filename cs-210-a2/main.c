74fc4c1bb5b8608bd1e17b40f8ebc75694b8e74e
Hao CHEN <ilff8chen@gmail.com>
2014-10-03 13:01:50 +1300

    Initial Commit

---
+++ cs-210-a2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  cs-210-a2
+//
+//  Created by Hao Chen on 3/10/14.
+//  Copyright (c) 2014 Hao Chen. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
